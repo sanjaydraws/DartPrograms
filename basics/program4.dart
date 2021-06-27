6f9687e7da8e23aeb00174c6cfe02f43e7e697a5
sanjaydraws <sprajapat8331@gmail.com>
2021-06-27 23:43:58 +0530

    function

---
+++ basics/program4.dart
@@ -0,0 +1,28 @@
+// operators 
+
+void main() {
+   int a = 23;
+   int b= 23;
+ 
+   String x = "sanjay";
+   String y = "sanjay";
+   print(a==b); // true 
+   print(x == y); // true 
+   print(x != y); // false 
+
+
+   // is -> true when object has specific type 
+   // is! -- false when the object has specific type  
+   String q = "Hello";
+   int r = 90;
+   print(r == q); //false 
+   print( q is String); //true 
+   print(r is !int); // false 
+
+
+
+
+
+
+  
+}
